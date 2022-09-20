c87c0c734d1813f79b1b55f3e0d088a3495d6058
ment360liveability <114008018+ment360liveability@users.noreply.github.com>
2022-09-21 01:39:41 +1000

    Update dataflow_backup.sh

--- dataflow/dataflow_backup.sh
+++ dataflow/dataflow_backup.sh
@@ -1,5 +1,5 @@
 #1.create a project and export it into a variable(project creation- manual)
-export PROJECT_ID="liveability-final"
+export PROJECT_ID="ment360liveability-final"
       
 #2.Sets the project,location &dataset
 gcloud config set project ${PROJECT_ID}
